4043a248dc9b8912189f86d0741cf015e844a104
Zain Afzal <zain.afz@gmail.com>
2018-03-08 08:25:31 +1100

    Set up basic structure for interfaces

--- bots/HodgePodge/modules/BotModule.py
+++
@@ -1,9 +0,0 @@
-class BotModule():
-    def __init__(self, name):
-        self.name = name.lower()
-    def getTriggerList(self):
-        return []
-    def connectDb(self, db):
-        return
-    async def respond(self, client, channel):
-        return
---
+++ interfaces/discordInterface.py
@@ -0,0 +1,7 @@
+# Create our boy
+Hp = HodgePodge()
+
+# Add in any modules
+
+
+# Activate Interface
