f82144d12a71c3937865aa76df8f8bf5a8f5408d
Zain Afzal <zain.afz@gmail.com>
2018-03-17 12:35:55 +1100

    Added dice rolling

--- modules/Game.py
+++ modules/Game.py
@@ -1,14 +1,58 @@
 from modules.ModuleDecorator import Trigger
 from modules.Module import Module
 from utils.Response import Response
+from utils.Formatter import Formatter
+import random
 
 # enforce the parser stuff
 class Game(Module):
     def __init__(self):
+        self.formater = Formatter()
+        self.diceTypeLimit = 1000
+        self.diceNumLimit = 1000
         super().__init__()
 
-    @Trigger('.*hodge podge.*roll.*a.*d\s*(\d+)',[])
+    def validDice(self, d):
+        if d < 0:
+            return False
+        if d > self.diceTypeLimit:
+            return False
+        return True
+
+    @Trigger('hodge podge.*roll.*a.*d\s*(\-?\d+)',[])
     def roll(self, context):
         res = Response()
-        res.textResponce("You got it! Haha i'm kidding you haven't coded that up yet fuckface",context["locationId"])
+        diceType = int(context["groups"][0])
+        if not self.validDice(diceType):
+            errMsg = "Sorry! I can't roll a d%d, I need a number >0 and <=%d"%(diceType,self.diceTypeLimit)
+            res.textResponce(errMsg,context["locationId"])
+        else:
+            diceRoll = random.randint(1,diceType)
+            rollStr = "I got %d!"%diceRoll
+            res.textResponce(rollStr,context["locationId"])
+        return (res,None)
+
+    @Trigger('hodge podge.*roll[^\d]*(\d+)[^d]*d\s*(\-?\d+)',[])
+    def multiroll(self, context):
+        res = Response()
+        diceNum = int(context["groups"][0])
+        diceType = int(context["groups"][1])
+        if not self.validDice(diceType):
+            errMsg = "Sorry! I can't roll a d%d, I need a number >0 and <=%d"%(diceType,self.diceTypeLimit)
+            res.textResponce(errMsg,context["locationId"])
+        elif diceNum < 0 or diceNum > self.diceNumLimit:
+            errMsg = "Sorry! I can't roll %d dice, I need a number >0 and <=%d"%(diceType,self.diceNumLimit)
+            res.textResponce(errMsg,context["locationId"])
+        else:
+            components = []
+            sum = 0
+            for i in range(diceNum):
+                d = random.randint(1,diceType)
+                sum += d
+                components.append(str(d))
+            rollStr = "I got %d!"%sum
+            componentStr = " (%s)"%("+".join(components))
+            if len(rollStr) + len(componentStr) < 2000:
+                rollStr += componentStr
+            res.textResponce(rollStr,context["locationId"])
         return (res,None)
---
+++ utils/Formatter.py
@@ -0,0 +1,31 @@
+class Formatter():
+    def __init__(self):
+        self.buffer = []
+    def output(self, s):
+        self.buffer.append(s)
+    def error(self, s):
+        self.buffer.append("_aww fuck_ %s"%s)
+    def list(self, l):
+        self.buffer.append("```")
+        for e in l:
+            self.buffer.append(" >  %s"%e)
+        self.buffer.append("```")
+    def multiList(self, l):
+        self.buffer.append("```")
+        for e in l:
+            self.buffer.append("%s"%e[0])
+            for subE in e[1:]:
+                if type(subE) is tuple:
+                    self.buffer.append("    %s %s"%(subE[0],subE[1]))
+                else:
+                    self.buffer.append("    >> %s"%subE)
+        self.buffer.append("```")
+    def generate(self):
+        o = "\n".join(self.buffer)
+        return o
+    def clear(self):
+        self.buffer.clear()
+    def getLines(self):
+        return len(self.buffer)
+    def getLen(self):
+        return len(self.generate())
